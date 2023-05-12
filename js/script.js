7630d3c357cd2891cc1b30bc921b7bdbdb477d1f
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-12 15:04:51 +0000

    Fixd comments, but need to fix indentation

--- js/script.js
+++ js/script.js
@@ -8,33 +8,37 @@
 
 function integersSent() {
 
-  //Initializing product variable and getting numbers from user
+  // Initializing product variable.
   let product = 0;
+
+  // Getting numbers from user.
   let firstNumber = parseInt(document.getElementById("first-number").value);
   let secondNumber = parseInt(document.getElementById("second-number").value);
 
-  //If statement to give an error message if nothing nothing is entered or if numbers are invalid
+  // If statement to give an error message if nothing nothing is entered or if numbers are invalid.
   if (isNaN(firstNumber) || isNaN(secondNumber)) {
     document.getElementById("results").innerHTML = "Please enter valid numbers.";
     return;
   }
   
-  //Else (for if numbers are entered)
+  // Else (for if numbers are entered)
   else {
+
     // Take the absolute value to do multiplication
     let firstNumberAbsolute = Math.abs(firstNumber);
     let secondNumberAbsolute = Math.abs(secondNumber);
-    //For loop to execute repeated addition that will result in the product.
+
+    // For loop to execute repeated addition that will result in the product.
     for (let counter = 0; counter < secondNumberAbsolute; counter++) {
       product = product + firstNumberAbsolute;
     }
   }
 
-  //If statement to make sure that if one of the numbers is negative, the product will be negative
+  // If statement to make sure that if one of the numbers is negative, the product will be negative
   if ((firstNumber < 0 && secondNumber > 0) || (firstNumber > 0 && secondNumber < 0)) {
     product = product * -1;
   }
 
-  //Displaying the product of the numbers to the user
+  // Displaying the product of the numbers to the user
   document.getElementById("results").innerHTML = (firstNumber + " x " + secondNumber + " = " + product) + " .";
 }
